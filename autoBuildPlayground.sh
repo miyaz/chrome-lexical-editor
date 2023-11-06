238ead2b60eecda7cd605688083a2a0f84925450
miyaz <sinji_mg@yahoo.co.jp>
2023-11-06 19:57:38 +0900

    delete logo and header

--- autoBuildPlayground.sh
+++ autoBuildPlayground.sh
@@ -80,78 +80,8 @@ EOS
 
 patch -u vite.prod.config.js < ../vite.prod.config.js.patch
 
-cat <<EOS > ../Editor.tsx.patch
-@@ -74,6 +74,59 @@
- import ContentEditable from './ui/ContentEditable';
- import Placeholder from './ui/Placeholder';
-
-+import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
-+import { BLUR_COMMAND, FOCUS_COMMAND, CLEAR_HISTORY_COMMAND, COMMAND_PRIORITY_EDITOR } from "lexical";
-+import {exportFile} from '@lexical/file';
-+import {version} from '../package.json';
-+import type {EditorState} from 'lexical';
-+
-+type DocumentJSON = {
-+  editorState: EditorState;
-+  lastSaved: number;
-+  version: typeof version;
-+};
-+
-+const EditorFocusBlurPlugin = () => {
-+  const [editor] = useLexicalComposerContext();
-+  useEffect(() => {
-+    editor.registerCommand(
-+      BLUR_COMMAND,
-+      (payload) => {
-+        console.log(payload);
-+        const now = new Date();
-+        const editorState = editor.getEditorState();
-+        const documentJSON: DocumentJSON = {
-+          editorState: editorState.toJSON(),
-+          lastSaved: now.getTime(),
-+          version,
-+        };
-+        chrome.storage.local.set({
-+          saveData: documentJSON,
-+        })
-+        .then(() => {
-+          console.log("Value is set");
-+        })
-+        .catch((e) => {
-+          console.log(e);
-+        });
-+      },
-+      COMMAND_PRIORITY_EDITOR
-+    );
-+
-+    chrome.storage.local.get(['saveData'], (res) => {
-+      console.log(res)
-+      //const json = JSON.parse(res);
-+      const editorState = editor.parseEditorState(
-+        JSON.stringify(res.saveData.editorState),
-+      );
-+      editor.setEditorState(editorState);
-+      editor.dispatchCommand(CLEAR_HISTORY_COMMAND, undefined);
-+    });
-+  }, []);
-+
-+  return null;
-+};
-+
- const skipCollaborationInit =
-   // @ts-ignore
-   window.parent != null && window.parent.frames.right === window;
-@@ -154,6 +207,7 @@
-         <ComponentPickerPlugin />
-         <EmojiPickerPlugin />
-         <AutoEmbedPlugin />
-+        <EditorFocusBlurPlugin />
-
-         <MentionsPlugin />
-         <EmojisPlugin />
-EOS
-
-patch -u src/Editor.tsx < ../Editor.tsx.patch
+cp -f ../src/Editor.tsx src/
+cp -f ../src/App.tsx src/
 
 npm run build-prod
 #npm run build-dev
